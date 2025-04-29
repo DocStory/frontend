5edc050a2d8b20ba616cd2dfe950c65ad001c928
kanguk <lm0828@naver.com>
2025-04-29 23:50:35 +0900

    feat: 라우터 설정
    /은 랜딩페이지, /repo-tree는 히스토리 트리페이지

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { GlobalStyle } from "./styles/GlobalStyle.ts";
 import LandingPage from './components/common/LandingPage';
+import RepoTreePage from './components/pages/RepoTreePage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <LandingPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/repo-tree" element={<RepoTreePage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
