1f994cc950493cd885d558d38454444f0c33ffc3
kanguk <lm0828@naver.com>
2025-05-09 20:12:13 +0900

    feat: 히스토리 상세 페이지 제작

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { GlobalStyle } from "./styles/GlobalStyle.ts";
 import LandingPage from './components/common/LandingPage';
 import RepoTreePage from './components/pages/RepoTreePage';
 import PhysicsGraphTestPage from './components/pages/PhysicsGraphTestPage';
+import RepositoryHistoryPage from './components/pages/RepositoryHistoryPage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/repo-tree" element={<RepoTreePage />} />
           <Route path="/physics-test" element={<PhysicsGraphTestPage />} />
+          <Route path="/repository-history" element={<RepositoryHistoryPage />} />
         </Routes>
       </BrowserRouter>
     </>
