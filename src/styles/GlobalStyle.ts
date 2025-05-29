641503254d621f121e1fc1941b050b23694ca963
kanguk <lm0828@naver.com>
2025-05-29 17:55:30 +0900

    fix: 폰트가 계속 리로드되던 문제 해결

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-  @import url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css');
-
   html, body, * {
     font-family: 'Pretendard', 'Inter', sans-serif;
     margin: 0;
